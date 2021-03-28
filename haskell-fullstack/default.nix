25500fec62a87ff5f3776d45a4c16a307f00c59c
Dan Dart <git@dandart.co.uk>
2021-03-28 17:22:37 +0100

    go back to unstable where appropriate

--- haskell-fullstack/default.nix
+++ haskell-fullstack/default.nix
@@ -1,4 +1,4 @@
-{ system ? builtins.currentSystem, nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz) {} }:
+{ system ? builtins.currentSystem, nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {} }:
 (import ./external/reflex-platform {
   inherit system;
   config.android_sdk.accept_license = true;  
